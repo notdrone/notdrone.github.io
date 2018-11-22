d679ce48e0399811a8f92fdbc10cbe26374abf4d
notdrone <notdrone@gmail.com>
2018-11-22 19:38:53 +0530

    change title and add dummy text

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'notdr.one',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,13 +6,15 @@ import Image from '../components/image'
 
 const IndexPage = () => (
   <Layout>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <nav>
+      <Link to="/page-2/">Go to page 2</Link>
+    </nav>
+    <div>logo</div>
+    <h1>Hi! I'm Droan Malik</h1>
+    <p>I do web stuff.</p>
     <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
       <Image />
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
 
